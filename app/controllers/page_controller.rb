a59073f35e7cdf8ccde9790406cb4000cc13b4f7
Saneery <anatolyniky@gmail.com>
2015-12-11 20:33:36 +0300

    Fresh heroku

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,16 +2,16 @@ require 'will_paginate/array'
 
 class PageController < ApplicationController
 	def index
-		search = params[:search]
+		search = params[:search].mb_chars.downcase.to_s
 	    if search!=''
 	      if params[:type]=='food'
-	  	    @objects = Food.tagged_with(search.downcase).sort_by_rating + Food.where(name: search.downcase).sort_by_rating
+	  	    @objects = Food.tagged_with(search).sort_by_rating + Food.where(name: search).sort_by_rating
 	  	    @objects.uniq!
 	  	    @objects.select!{|o| o.publick.approve == true }
 	        @objects = @objects.paginate(per_page: 10, page: params[:page])
 	        render "foods/index"
 	      elsif params[:type]=='publick'
-	        @objects = Publick.where(name: search.downcase).sort_by_rating
+	        @objects = Publick.where(name: search).sort_by_rating
 	        @objects.select!{|o| o.approve == true}
 	        @objects = @objects.paginate(per_page: 10, page: params[:page])
 	        render "publicks/index"
